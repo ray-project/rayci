ccad1c0ab4eecc75182c3306de99cb1f35e055cd
Kai Fricke <kai@anyscale.com>
2022-09-15 16:02:15 +0100

    Rename files

--- ray_ci/build_base_build.sh
+++ ray_ci/build_base_build.sh
@@ -2,8 +2,10 @@ set -e
 
 cd "$RAY_REPO_DIR" || true
 
-export DOCKER_IMAGE_BASE=$ECR_REPO:oss-ci-base_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-base_latest
+export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_BRANCH/\//_}
+
+export DOCKER_IMAGE_BASE_BUILD=$ECR_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
+export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-base_build_latest_$BUILDKITE_BRANCH_CLEAN
 
 echo "--- :docker: Building base dependency image for BUILDS"
 date +"%Y-%m-%d %H:%M:%S"
@@ -13,16 +15,16 @@ time docker build \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
-  -t "$DOCKER_IMAGE_BASE" \
+  -t "$DOCKER_IMAGE_BASE_BUILD" \
   -t "$DOCKER_IMAGE_TAG" \
-  -f ci/docker/Dockerfile.base .
+  -f ci/docker/Dockerfile.base_build .
 
 date +"%Y-%m-%d %H:%M:%S"
 
 echo "--- :arrow_up: Pushing docker image to ECR"
 date +"%Y-%m-%d %H:%M:%S"
 
-time docker push "$DOCKER_IMAGE_BASE"
+time docker push "$DOCKER_IMAGE_BASE_BUILD"
 time docker push "$DOCKER_IMAGE_TAG"
 
 date +"%Y-%m-%d %H:%M:%S"
--- ray_ci/build_base_test.sh
+++ ray_ci/build_base_test.sh
@@ -2,8 +2,10 @@ set -e
 
 cd "$RAY_REPO_DIR" || true
 
-export DOCKER_IMAGE_TEST_BASE=$ECR_REPO:oss-ci-test_base_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-test_base_latest
+export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_BRANCH/\//_}
+
+export DOCKER_IMAGE_BASE_TEST=$ECR_REPO:oss-ci-base_test_$BUILDKITE_COMMIT
+export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-base_test_latest_$BUILDKITE_BRANCH_CLEAN
 
 echo "--- :docker: Building base dependency image for TESTS"
 date +"%Y-%m-%d %H:%M:%S"
@@ -13,16 +15,16 @@ time docker build \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
-  -t "$DOCKER_IMAGE_TEST_BASE" \
+  -t "$DOCKER_IMAGE_BASE_TEST" \
   -t "$DOCKER_IMAGE_TAG" \
-  -f ci/docker/Dockerfile.test_base .
+  -f ci/docker/Dockerfile.base_test .
 
 date +"%Y-%m-%d %H:%M:%S"
 
 echo "--- :arrow_up: Pushing docker image to ECR"
 date +"%Y-%m-%d %H:%M:%S"
 
-time docker push "$DOCKER_IMAGE_TEST_BASE"
+time docker push "$DOCKER_IMAGE_BASE_TEST"
 time docker push "$DOCKER_IMAGE_TAG"
 
 date +"%Y-%m-%d %H:%M:%S"
