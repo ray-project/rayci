ce7e31d7f73513deeb76297a5eed7b96203c8034
Kai Fricke <kai@anyscale.com>
2022-09-15 21:21:04 +0100

    Build base + GPU

--- ray_ci/build_base_build.sh
+++ ray_ci/build_base_build.sh
@@ -4,10 +4,13 @@ cd "$RAY_REPO_DIR" || true
 
 export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_BRANCH/\//_}
 
-export DOCKER_IMAGE_BASE_BUILD=$ECR_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-base_build_latest_$BUILDKITE_BRANCH_CLEAN
+export DOCKER_IMAGE_BASE_TEST=$ECR_REPO:oss-ci-base_test_$BUILDKITE_COMMIT
+export DOCKER_IMAGE_TAG_TEST=$ECR_REPO:oss-ci-base_test_latest_$BUILDKITE_BRANCH_CLEAN
 
-echo "--- :docker: Building base dependency image for BUILDS"
+export DOCKER_IMAGE_BASE_BUILD=$ECR_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
+export DOCKER_IMAGE_TAG_BUILD=$ECR_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
+
+echo "--- :docker: Building base dependency image for TESTS :python:"
 date +"%Y-%m-%d %H:%M:%S"
 
 time docker build \
@@ -15,16 +18,32 @@ time docker build \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
+  -t "$DOCKER_IMAGE_BASE_TEST" \
+  -t "$DOCKER_IMAGE_TAG_TEST" \
+  -f ci/docker/Dockerfile.base_test .
+
+echo "--- :docker: Building base dependency image for BUILDS :gear:"
+date +"%Y-%m-%d %H:%M:%S"
+
+time docker build \
+  --build-arg REMOTE_CACHE_URL \
+  --build-arg BUILDKITE_PULL_REQUEST \
+  --build-arg BUILDKITE_COMMIT \
+  --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
+  --build-arg DOCKER_IMAGE_BASE_TEST \
   -t "$DOCKER_IMAGE_BASE_BUILD" \
-  -t "$DOCKER_IMAGE_TAG" \
+  -t "$DOCKER_IMAGE_TAG_BUILD" \
   -f ci/docker/Dockerfile.base_build .
 
 date +"%Y-%m-%d %H:%M:%S"
 
-echo "--- :arrow_up: Pushing docker image to ECR"
+echo "--- :arrow_up: Pushing docker images to ECR"
 date +"%Y-%m-%d %H:%M:%S"
 
 time docker push "$DOCKER_IMAGE_BASE_BUILD"
-time docker push "$DOCKER_IMAGE_TAG"
+time docker push "$DOCKER_IMAGE_TAG_BUILD"
+
+time docker push "$DOCKER_IMAGE_BASE_TEST"
+time docker push "$DOCKER_IMAGE_TAG_TEST"
 
 date +"%Y-%m-%d %H:%M:%S"
--- ray_ci/build_base_gpu.sh
+++ ray_ci/build_base_gpu.sh
@@ -4,10 +4,10 @@ cd "$RAY_REPO_DIR" || true
 
 export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_BRANCH/\//_}
 
-export DOCKER_IMAGE_BASE_TEST=$ECR_REPO:oss-ci-base_test_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG=$ECR_REPO:oss-ci-base_test_latest_$BUILDKITE_BRANCH_CLEAN
+export DOCKER_IMAGE_BASE_GPU=$ECR_REPO:oss-ci-base_gpu_$BUILDKITE_COMMIT
+export DOCKER_IMAGE_TAG_GPU=$ECR_REPO:oss-ci-base_gpu_latest_$BUILDKITE_BRANCH_CLEAN
 
-echo "--- :docker: Building base dependency image for TESTS"
+echo "--- :docker: Building base dependency image for GPU tests :tv:"
 date +"%Y-%m-%d %H:%M:%S"
 
 time docker build \
@@ -15,16 +15,16 @@ time docker build \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
-  -t "$DOCKER_IMAGE_BASE_TEST" \
-  -t "$DOCKER_IMAGE_TAG" \
-  -f ci/docker/Dockerfile.base_test .
+  -t "$DOCKER_IMAGE_BASE_GPU" \
+  -t "$DOCKER_IMAGE_TAG_GPU" \
+  -f ci/docker/Dockerfile.base_gpu .
 
 date +"%Y-%m-%d %H:%M:%S"
 
 echo "--- :arrow_up: Pushing docker image to ECR"
 date +"%Y-%m-%d %H:%M:%S"
 
-time docker push "$DOCKER_IMAGE_BASE_TEST"
-time docker push "$DOCKER_IMAGE_TAG"
+time docker push "$DOCKER_IMAGE_BASE_GPU"
+time docker push "$DOCKER_IMAGE_TAG_GPU"
 
 date +"%Y-%m-%d %H:%M:%S"
