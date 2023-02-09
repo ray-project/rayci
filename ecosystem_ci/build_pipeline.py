3c00ff57f63c0dc2f83ca51c7d81fae02fc1c8d2
Kai Fricke <kai@anyscale.com>
2023-02-08 16:03:17 -0800

    Add pipeline builder for ecosystem runners

---
+++ ecosystem_ci/build_pipeline.py
@@ -0,0 +1,86 @@
+import copy
+import json
+import os
+from pathlib import Path
+from typing import Dict, List
+
+import click
+import yaml
+
+
+BASE_STEPS_JSON = Path(__file__).parent / "step.json"
+
+
+ALLOW_STEP_OVERRIDE = {"label", "commands", "if"}
+
+SPECIAL_FIELD_IMAGE = "image"
+SPECIAL_FIELD_INSTANCE_SIZE = "instance_size"
+
+DEFAULT_INSTANCE_SIZE = "large"
+
+INSTANCE_SIZE_TO_QUEUE = {
+    "small": os.environ.get("RUNNER_QUEUE_SMALL", "__runner_queue_small"),
+    "medium": os.environ.get("RUNNER_QUEUE_MEDIUM", "__runner_queue_medium"),
+    "large": os.environ.get("RUNNER_QUEUE_LARGE", "__runner_queue_large"),
+    "gpu": os.environ.get("RUNNER_QUEUE_GPU", "__runner_queue_gpu"),
+}
+
+
+def load_pipeline_steps(base_step: Dict, pipeline_file_path: str) -> List[Dict]:
+    steps = []
+    with open(pipeline_file_path, "r") as fp:
+        jobs = yaml.safe_load(fp)
+
+    for job in jobs:
+        steps.append(job_to_step(base_step, job))
+
+    return steps
+
+
+def job_to_step(base_step: Dict, job: Dict) -> Dict:
+    step = copy.deepcopy(base_step)
+    job = job.copy()
+
+    image = job.pop(SPECIAL_FIELD_IMAGE, None)
+    if image:
+        docker_key = list(base_step["plugins"][0].keys())[0]
+        step["plugins"][0][docker_key]["image"] = image
+
+    instance_size = job.pop(SPECIAL_FIELD_INSTANCE_SIZE, DEFAULT_INSTANCE_SIZE)
+    try:
+        step["agents"]["queue"] = INSTANCE_SIZE_TO_QUEUE[instance_size]
+    except Exception as e:
+        raise ValueError(
+            f"Invalid instance size: {instance_size}. Choose from "
+            f"{list(INSTANCE_SIZE_TO_QUEUE.keys())}"
+        ) from e
+
+    for key, val in job.items():
+        if key not in ALLOW_STEP_OVERRIDE:
+            raise ValueError(f"Cannot override field: {key}")
+
+        step[key] = val
+
+    return step
+
+
+@click.command()
+@click.argument("pipelines", required=True, type=str, nargs=-1)
+def main(
+    pipelines: List[str],
+):
+    with open(BASE_STEPS_JSON, "r") as f:
+        base_step = json.load(f)
+
+    all_steps = []
+
+    for pipeline in pipelines:
+        all_steps += load_pipeline_steps(base_step, pipeline)
+
+    # Print to stdout
+    steps_str = json.dumps(all_steps)
+    print(steps_str)
+
+
+if __name__ == "__main__":
+    main()
