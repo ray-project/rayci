70474e150703e1ab5c13bc8a3a29f76c0a8e5c84
Kai Fricke <kai@anyscale.com>
2023-03-29 15:40:27 -0700

    default

--- ray_ci/bootstrap_mac.sh
+++ ray_ci/bootstrap_mac.sh
@@ -12,7 +12,7 @@ echo "Kicking off the full Mac OS pipeline"
 
 export RUNNER_QUEUE_ARM64_MEDIUM="$RUNNER_QUEUE_MAC_ARM64"
 
-python3 "${PIPELINE_REPO_DIR}/ray_ci/pipeline_ci.py" --queue "$RUNNER_QUEUE_MAC_DEFAULT" \
+python3 "${PIPELINE_REPO_DIR}/ray_ci/pipeline_ci.py" --queue "$RUNNER_QUEUE_DEFAULT" \
   --base-step-file "${PIPELINE_REPO_DIR}/ray_ci/step_macos.json" \
   "/Users/kai/coding/ray/.buildkite/pipeline.macos.yml" > pipeline.txt
 
