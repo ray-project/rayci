6a052018a5942dea3715b0f4ee26e2d46f28139a
Kai Fricke <kai@anyscale.com>
2023-03-29 15:52:05 -0700

    remove env export

--- ray_ci/bootstrap_mac.sh
+++ ray_ci/bootstrap_mac.sh
@@ -11,8 +11,6 @@ env
 echo "--- :rocket: Launching Mac OS tests :gear:"
 echo "Kicking off the full Mac OS pipeline"
 
-export RUNNER_QUEUE_ARM64_MEDIUM="$RUNNER_QUEUE_MAC_ARM64"
-
 python3 "${PIPELINE_REPO_DIR}/ray_ci/pipeline_ci.py" --queue "$RUNNER_QUEUE_DEFAULT" \
   --base-step-file "${PIPELINE_REPO_DIR}/ray_ci/step_macos.json" \
   "./.buildkite/pipeline.macos.yml" > pipeline.txt
