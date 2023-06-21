4e1544bcdccf1a70bff7577ee467e5d0ea989244
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-06-21 10:33:13 -0700

    Revert "Add grouping support for ray CI pipeline."

--- ray_ci/pipeline_ci.py
+++ ray_ci/pipeline_ci.py
@@ -56,22 +56,13 @@ def read_pipeline(pipeline_path: Path):
         return []
 
     with open(pipeline_path, "r") as f:
-        content = f.read()
-
-    group_name = None
-    if content.startswith("#ci:group="):
-        first_line = content.splitlines()[0]
-        group_name = first_line[len("#ci:group="):].strip()
-
-    steps = yaml.safe_load(content)
-    if not steps:
-        steps = []
+        steps = yaml.safe_load(f)
 
     # E.g. the MacOS pipeline is a "full" yaml
-    if isinstance(steps, dict) and "steps" in steps:
+    if "steps" in steps:
         steps = steps["steps"]
 
-    return steps, group_name
+    return steps
 
 
 def filter_pipeline_conditions(
@@ -245,7 +236,7 @@ def main(
     assert queue
     assert not (early_only and not_early_only)
 
-    pipeline_steps, group_name = read_pipeline(pipeline_path)
+    pipeline_steps = read_pipeline(pipeline_path)
 
     # Filter early kick-off
     if early_only:
@@ -303,13 +294,7 @@ def main(
         pipeline_steps = inject_commands(pipeline_steps, before=setup_commands)
 
     # Print to stdout
-    if group_name:
-        pipeline_steps = {
-            "group": group_name,
-            "steps": pipeline_steps,
-        }
-
-    steps_str = json.dumps({"steps:": pipeline_steps})
+    steps_str = json.dumps(pipeline_steps)
     print(steps_str)
 
 
