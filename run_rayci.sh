cca3dabebafc4230a9737efafb48bcc7c6b50b13
Lonnie Liu <lonnie@anyscale.com>
2023-07-05 23:44:48 +0000

    .

--- run_rayci.sh
+++ run_rayci.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 
 RAYCI_BRANCH="${RAYCI_BRANCH:-stable}"
 
+TMP_DIR="$(mktemp -d)"
+
 echo "--- Install rayci"
 
-curl -sL 'https://go.dev/dl/go1.20.5.linux-amd64.tar.gz' | tar -xzf - -C /usr/local
-/usr/local/go/bin/go install 'github.com/ray-project/rayci@'"${RAYCI_BRANCH}"
+curl -sL 'https://go.dev/dl/go1.20.5.linux-amd64.tar.gz' | tar -xzf - -C "$TMP_DIR"
+export GOROOT="$TMP_DIR/go"
+export GOHOME="$TMP_DIR/gohome"
+"$TMP_DIR/go/bin/go" install 'github.com/ray-project/rayci@'"${RAYCI_BRANCH}"
 
 echo "--- Run rayci"
 
-exec "$HOME/go/bin/rayci" "$@"
+exec "$GOHOME/bin/rayci" "$@"
