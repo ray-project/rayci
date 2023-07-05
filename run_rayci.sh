ea5052a7de986162da9cc8bbe4e9a90b30201e59
Lonnie Liu <lonnie@anyscale.com>
2023-07-05 23:12:52 +0000

    branch picking

--- run_rayci.sh
+++ run_rayci.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-RAY_CI_BRANCH="${1:-stable}"
+RAYCI_BRANCH="${RAYCI_BRANCH:-stable}"
 
 curl -sL 'https://go.dev/dl/go1.20.5.linux-amd64.tar.gz' | tar -xzf - -C /usr/local
-/usr/local/bin/go install 'github.com/ray-project/rayci@'"${RAY_CI_BRANCH}" -o /usr/local/bin/rayci
+/usr/local/bin/go install 'github.com/ray-project/rayci@'"${RAYCI_BRANCH}" -o /usr/local/bin/rayci
 
 exec rayci "$@"
