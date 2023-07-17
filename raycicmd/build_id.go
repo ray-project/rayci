2c64d16a68b2abda7cf3eac41998769d9c7f92f6
Lonnie Liu <lonnie@anyscale.com>
2023-07-17 00:32:41 +0000

    add build id too

---
+++ raycicmd/build_id.go
@@ -0,0 +1,28 @@
+package raycicmd
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+func makeBuildID(envs Envs) (string, error) {
+	buildID := getEnv(envs, "RAYCI_BUILD_ID")
+	if buildID != "" {
+		return buildID, nil
+	}
+
+	buildID = getEnv(envs, "BUILDKITE_BUILD_ID")
+	if buildID != "" {
+		h := sha256.Sum256([]byte(buildID))
+		prefix := hex.EncodeToString(h[:])[:8]
+		return prefix, nil
+	}
+
+	user := getEnv(envs, "USER")
+	if user != "" {
+		return user, nil
+	}
+
+	return "", fmt.Errorf("no build id found")
+}
