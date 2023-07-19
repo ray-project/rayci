b24b99333196cba371970989f77f0c7615ec324d
Lonnie Liu <lonnie@anyscale.com>
2023-07-19 03:19:36 +0000

    add config struct and parsing
    config for how to bootstrap a pipeline, with builtin
    config files for existing pipelines

---
+++ raycicmd/config.go
@@ -0,0 +1,134 @@
+package raycicmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	yaml "gopkg.in/yaml.v3"
+)
+
+type config struct {
+	name string
+
+	ArtifactsBucket string `yaml:"artifacts_bucket"`
+
+	CITemp     string `yaml:"ci_temp"`
+	CITempRepo string `yaml:"ci_temp_repo"`
+
+	BuilderQueues map[string]string `yaml:"builder_queues"`
+	RunnerQueues  map[string]string `yaml:"agent_queues"`
+
+	Dockerless bool `yaml:"dockerless"`
+
+	// BuildkiteDir is the directory of buildkite pipeline files.
+	BuildkiteDir string `yaml:"buildkite_dir"`
+
+	// ForgeDir is the directory of forge Dockerfile files.
+	ForgeDirs []string `yaml:"forge_dir"`
+}
+
+func localDefaultConfig(envs Envs) *config {
+	return &config{
+		CITemp: filepath.Join(getEnv(envs, "HOME"), ".cache/rayci"),
+	}
+}
+
+// builtin ray buildkite pipeline IDs.
+const (
+	rayBranchPipeline = "0183465b-c6fb-479b-8577-4cfd743b545d"
+
+	rayPRPipeline  = "0183465f-a222-467a-b122-3b9ea3e68094"
+	rayDevPipeline = "5b097a97-ad35-4443-9552-f5c413ead11c"
+)
+
+const rayCIECR = "029272617770.dkr.ecr.us-west-2.amazonaws.com"
+
+var defaultForgeDirs = []string{".buildkite/forge", "ci/forge", "ci/v2/forge"}
+
+var branchPipelineConfig = &config{
+	name: "ray-branch",
+
+	ArtifactsBucket: "ray-ci-artifact-branch-public",
+
+	CITemp:     "s3://ray-ci-artifact-branch-public/ci-temp/",
+	CITempRepo: rayCIECR + "/rayci_temp_branch",
+
+	BuilderQueues: map[string]string{
+		"builder":       "builder_queue_branch",
+		"builder-arm64": "builder_queue_arm64_branch",
+	},
+
+	RunnerQueues: map[string]string{
+		"default":   "runner_queue_branch",
+		"small":     "runner_queue_small_branch",
+		"medium":    "runner_queue_medium_branch",
+		"large":     "runner_queue_branch",
+		"gpu":       "gpu_runner_queue_branch",
+		"gpu-large": "gpu_large_runner_queue_branch",
+
+		"medium-arm64": "runner_queue_arm64_medium_branch",
+	},
+
+	ForgeDirs: defaultForgeDirs,
+}
+
+var prPipelineConfig = &config{
+	name: "ray-pr",
+
+	ArtifactsBucket: "ray-ci-artifact-pr-public",
+
+	CITemp:     "s3://ray-ci-artifact-pr-public/ci-temp/",
+	CITempRepo: rayCIECR + "/rayci_temp_pr",
+
+	BuilderQueues: map[string]string{
+		"builder":       "builder_queue_pr",
+		"builder-arm64": "builder_queue_arm64_pr",
+	},
+
+	RunnerQueues: map[string]string{
+		"default":   "runner_queue_pr",
+		"small":     "runner_queue_small_pr",
+		"medium":    "runner_queue_medium_pr",
+		"large":     "runner_queue_pr",
+		"gpu":       "gpu_runner_queue_pr",
+		"gpu-large": "gpu_large_runner_queue_pr",
+
+		"medium-arm64": "runner_queue_arm64_medium_pr",
+	},
+
+	ForgeDirs: defaultForgeDirs,
+}
+
+func ciDefaultConfig(envs Envs) *config {
+	pipelineID := getEnv(envs, "BUILDKITE_PIPELINE_ID")
+	if pipelineID == rayBranchPipeline {
+		return branchPipelineConfig
+	}
+	return prPipelineConfig
+}
+
+func defaultConfig(envs Envs) *config {
+	envCI := getEnv(envs, "CI")
+	if envCI == "true" || envCI == "1" {
+		return ciDefaultConfig(envs)
+	}
+	return localDefaultConfig(envs)
+}
+
+func loadConfig(f string, envs Envs) (*config, error) {
+	if f == "" {
+		return defaultConfig(envs), nil
+	}
+
+	bs, err := os.ReadFile(f)
+	if err != nil {
+		return nil, fmt.Errorf("read config: %w", err)
+	}
+
+	config := new(config)
+	if err := yaml.Unmarshal(bs, config); err != nil {
+		return nil, fmt.Errorf("unmarshal config: %w", err)
+	}
+	return config, nil
+}
---
+++ raycicmd/config_test.go
@@ -0,0 +1,73 @@
+package raycicmd
+
+import (
+	"testing"
+
+	"os"
+	"path/filepath"
+)
+
+func TestLoadConfig(t *testing.T) {
+	t.Run("load ray branch CI config", func(t *testing.T) {
+		envs := newEnvsMap(map[string]string{
+			"CI":                    "true",
+			"BUILDKITE_PIPELINE_ID": rayBranchPipeline,
+		})
+
+		config, err := loadConfig("", envs)
+		if err != nil {
+			t.Fatalf("load config: %v", err)
+		}
+		if want := "ray-branch"; config.name != want {
+			t.Errorf("config got %q, want %q", config.name, want)
+		}
+	})
+
+	t.Run("load ray PR CI config", func(t *testing.T) {
+		envs := newEnvsMap(map[string]string{
+			"CI":                    "true",
+			"BUILDKITE_PIPELINE_ID": rayPRPipeline,
+		})
+
+		config, err := loadConfig("", envs)
+		if err != nil {
+			t.Fatalf("load config: %v", err)
+		}
+		if want := "ray-pr"; config.name != want {
+			t.Errorf("config got %q, want %q", config.name, want)
+		}
+	})
+
+	t.Run("load config file", func(t *testing.T) {
+		tmp := t.TempDir()
+		const bs = `ci_temp: "s3://fake/ci-temp"`
+		file := filepath.Join(tmp, "config.yaml")
+		if err := os.WriteFile(file, []byte(bs), 0600); err != nil {
+			t.Fatalf("write config: %v", err)
+		}
+
+		config, err := loadConfig(file, &osEnvs{})
+		if err != nil {
+			t.Fatalf("load config: %v", err)
+		}
+		const ciTemp = "s3://fake/ci-temp"
+		if config.CITemp != ciTemp {
+			t.Errorf("config got %q, want %q", config.CITemp, ciTemp)
+		}
+	})
+
+	t.Run("load local config", func(t *testing.T) {
+		envs := newEnvsMap(map[string]string{
+			"HOME": "/opt/fakehome",
+		})
+
+		config, err := loadConfig("", envs)
+		if err != nil {
+			t.Fatalf("load config: %v", err)
+		}
+		const ciTemp = "/opt/fakehome/.cache/rayci"
+		if config.CITemp != ciTemp {
+			t.Errorf("config got %q, want %q", config.CITemp, ciTemp)
+		}
+	})
+}
