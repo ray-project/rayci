07c4ef9e0e3f2a0c579459bc14d931a853b9f439
Lonnie Liu <lonnie@anyscale.com>
2023-07-27 01:10:45 +0000

    add more stuff for pipeline defs

--- raycicmd/bk_pipeline.go
+++ raycicmd/bk_pipeline.go
@@ -1,5 +1,9 @@
 package raycicmd
 
+import (
+	"time"
+)
+
 type bkPipelineGroup struct {
 	Group string `yaml:"group,omitempty"`
 	Key   string `yaml:"key,omitempty"`
@@ -27,3 +31,46 @@ func makeNoopBkPipeline(q string) *bkPipeline {
 		}},
 	}
 }
+
+func makeRayDockerPlugin(image string, extraEnvs []string) map[string]any {
+	return map[string]any{
+		"image":         image,
+		"shell":         []string{"/bin/bash", "-elic"},
+		"workdir":       "/ray",
+		"add-caps":      []string{"SYS_PTRACE", "SYS_ADMIN", "NET_ADMIN"},
+		"security-opts": []string{"apparmor=unconfined"},
+
+		"volumes": []string{"/var/run/docker.sock:/var/run/docker.sock"},
+
+		"environment": append([]string{
+			"CI",
+			"BUILDKITE",
+			"BUILDKITE_BRANCH",
+			"BUILDKITE_COMMIT",
+			"BUILDKITE_LABEL",
+			"BUILDKITE_PIPELINE_ID",
+			"BUILDKITE_PIPELINE_SLUG",
+			"BUILDKITE_BUILD_ID",
+			"BUILDKITE_BUILD_NUMBER",
+			"BUILDKITE_BUILD_URL",
+			"BUILDKITE_JOB_ID",
+			"BUILDKITE_PARALLEL_JOB",
+			"BUILDKITE_PARALLEL_JOB_COUNT",
+			"BUILDKITE_MESSAGE",
+		}, extraEnvs...),
+	}
+}
+
+var (
+	defaultRayRetry = map[string]any{
+		"manual": map[string]any{"permit_on_passed": true},
+		"automatic": []any{
+			map[string]any{"exit_status": -1, "limit": 3},
+			map[string]any{"exit_status": 255, "limit": 3},
+		},
+	}
+
+	defaultTimeoutInMinutes = int((5 * time.Hour).Minutes())
+
+	defaultArtifactPaths = []string{"tmp/artifacts/**/*"}
+)
