454531b72896fb392c4fedf856f7747726092a31
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-02 09:28:40 -0700

    func that checks if it is rayci pipeline file (#46)
    a rayci yaml file is a file that ends with ".rayci.yaml" or
    ".rayci.yml".

--- raycicmd/make.go
+++ raycicmd/make.go
@@ -1,5 +1,65 @@
 package raycicmd
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+func isRayCIYaml(p string) bool {
+	if strings.HasSuffix(p, ".rayci.yaml") {
+		return true
+	}
+	if strings.HasSuffix(p, ".rayci.yml") {
+		return true
+	}
+	return false
+}
+
+func listCIYamlFiles(dir string) ([]string, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			entries = nil
+		} else {
+			return nil, fmt.Errorf("read pipeline dir: %w", err)
+		}
+	}
+
+	var names []string
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		name := entry.Name()
+		if !isRayCIYaml(name) {
+			continue
+		}
+		names = append(names, name)
+	}
+
+	return names, nil
+}
+
+func parsePipelineFile(file string) (*pipelineGroup, error) {
+	bs, err := os.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("read pipeline file: %w", err)
+	}
+
+	g := new(pipelineGroup)
+	dec := yaml.NewDecoder(bytes.NewReader(bs))
+	dec.KnownFields(true)
+	if err := dec.Decode(g); err != nil {
+		return nil, fmt.Errorf("unmarshal pipeline file: %w", err)
+	}
+
+	return g, nil
+}
+
 func makePipeline(repoDir string, config *config, buildID string) (
 	*bkPipeline, error,
 ) {
---
+++ raycicmd/make_test.go
@@ -0,0 +1,142 @@
+package raycicmd
+
+import (
+	"testing"
+
+	"bytes"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"reflect"
+)
+
+func TestIsRayCIYaml(t *testing.T) {
+	for _, f := range []string{
+		"foo.rayci.yaml",
+		"foo.rayci.yml",
+		"dir/foo.rayci.yml",
+	} {
+		if !isRayCIYaml(f) {
+			t.Errorf("want %q to be a rayci yaml", f)
+		}
+	}
+
+	for _, f := range []string{
+		"rayci.yaml",
+		"pipeline.build.yaml",
+		"pipeline.tests.yml",
+	} {
+		if isRayCIYaml(f) {
+			t.Errorf("want %q to not be a rayci yaml", f)
+		}
+	}
+}
+
+func TestListCIYamlFiles(t *testing.T) {
+	tmp := t.TempDir()
+
+	for _, f := range []string{
+		"foo.rayci.yaml",
+		"bar.rayci.yaml",
+		"foo.rayci.yml",
+		"dir/foo.rayci.yml",
+		"pipeline.build.yaml",
+	} {
+		dir := filepath.Join(tmp, filepath.Dir(f))
+		if err := os.MkdirAll(dir, 0o700); err != nil {
+			t.Fatalf("mkdir for %q: %v", f, err)
+		}
+
+		if err := os.WriteFile(filepath.Join(tmp, f), nil, 0o600); err != nil {
+			t.Fatalf("write file %q: %v", f, err)
+		}
+	}
+
+	got, err := listCIYamlFiles(tmp)
+	if err != nil {
+		t.Fatalf("listCIYamlFiles: %v", err)
+	}
+
+	want := []string{
+		"bar.rayci.yaml",
+		"foo.rayci.yaml",
+		"foo.rayci.yml",
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+const goodTestPipeline = `
+group: g
+key: k
+steps:
+  - label: "test1"
+    key: "test1"
+    commands: [ "echo test1" ]
+  - label: "test2"
+    key: "test2"
+    commands: [ "echo test2" ]
+`
+
+const badTestPipeline = `
+name: n
+key: k
+steps:
+	- label: "test1"
+`
+
+func TestParsePipelineFile(t *testing.T) {
+	t.Run("good", func(t *testing.T) {
+		tmp := t.TempDir()
+		p := filepath.Join(tmp, "pipeline.yaml")
+		if err := os.WriteFile(p, []byte(goodTestPipeline), 0o600); err != nil {
+			t.Fatalf("write pipeline file: %v", err)
+		}
+
+		g, err := parsePipelineFile(p)
+		if err != nil {
+			t.Fatalf("parsePipelineFile: %v", err)
+		}
+
+		want := &pipelineGroup{
+			Group: "g",
+			Key:   "k",
+			Steps: []map[string]any{{
+				"label":    "test1",
+				"key":      "test1",
+				"commands": []string{"echo test1"},
+			}, {
+				"label":    "test2",
+				"key":      "test2",
+				"commands": []string{"echo test2"},
+			}},
+		}
+
+		gotJSON, err := json.MarshalIndent(g, "", "  ")
+		if err != nil {
+			t.Fatalf("json marshal got : %v", err)
+		}
+
+		wantJSON, err := json.MarshalIndent(want, "", "  ")
+		if err != nil {
+			t.Fatalf("json marshal want: %v", err)
+		}
+
+		if !bytes.Equal(gotJSON, wantJSON) {
+			t.Errorf("got %s\n, want %s", gotJSON, wantJSON)
+		}
+	})
+
+	t.Run("bad", func(t *testing.T) {
+		tmp := t.TempDir()
+		p := filepath.Join(tmp, "pipeline.yaml")
+		if err := os.WriteFile(p, []byte(badTestPipeline), 0o600); err != nil {
+			t.Fatalf("write pipeline file: %v", err)
+		}
+
+		if _, err := parsePipelineFile(p); err == nil {
+			t.Fatalf("parsePipelineFile: got nil, want error")
+		}
+	})
+}
