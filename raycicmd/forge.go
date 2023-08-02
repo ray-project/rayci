f57f9efaeb260be44117e349a88eef85d85c3a3a
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-02 09:28:29 -0700

    add forge building (#45)
    this is a special type of build jobs where the command line is fixed. it
    builds a base container with no input in the builder queue, called
    forges, and allows other runner rules to use these forge containers to
    run other jobs, such as building containers.
    
    it is only for bootstraping the build process with a standard build job
    sandbox. forges will not only contain build tools, but not
    library/application/build target dependencies, and hence does not need
    input.

--- raycicmd/forge.go
+++ raycicmd/forge.go
@@ -1,6 +1,9 @@
 package raycicmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -15,4 +18,76 @@ func forgeNameFromDockerfile(name string) (string, bool) {
 		return "", false
 	}
 	return name, true
+}
+
+// builtin builder command to build a forge container image.
+const forgeBuilderCommand = `/bin/bash -euo pipefail -c ` +
+	`'export DOCKER_BUILDKIT=1 ; ` +
+	`DEST_IMG="$${RAYCI_TMP_REPO}:$${RAYCI_BUILD_ID}-$${RAYCI_FORGE_NAME}" ; ` +
+	`tar --mtime="UTC 2020-01-01" -c -f - "$${RAYCI_FORGE_DOCKERFILE}" |` +
+	` docker build --progress=plain -t "$${DEST_IMG}" ` +
+	` -f "$${RAYCI_FORGE_DOCKERFILE}" - ; ` +
+	`docker push "$${DEST_IMG}" '`
+
+func makeForgeStep(buildID, name, file string, config *config) map[string]any {
+	agent := ""
+	if config.BuilderQueues != nil {
+		if q, ok := config.BuilderQueues["builder"]; ok {
+			agent = q
+		}
+	}
+
+	bkStep := map[string]any{
+		"label":    name,
+		"key":      name,
+		"commands": []string{forgeBuilderCommand},
+		"env": map[string]string{
+			"RAYCI_BUILD_ID":         buildID,
+			"RAYCI_TMP_REPO":         config.CITempRepo,
+			"RAYCI_FORGE_DOCKERFILE": file,
+			"RAYCI_FORGE_NAME":       name,
+		},
+	}
+	if agent != "" {
+		bkStep["agents"] = newBkAgents(agent)
+	}
+
+	return bkStep
+}
+
+func makeForgeGroup(repoDir, buildID string, config *config) (
+	*bkPipelineGroup, error,
+) {
+	g := &bkPipelineGroup{
+		Group: "forge",
+		Key:   "all-forges",
+	}
+
+	// add forge container building steps
+	for _, dir := range config.ForgeDirs {
+		entries, err := os.ReadDir(filepath.Join(repoDir, dir))
+		if err != nil {
+			if os.IsNotExist(err) {
+				continue
+			}
+			return nil, fmt.Errorf("read forge dir %s: %w", dir, err)
+		}
+
+		for _, entry := range entries {
+			if entry.IsDir() {
+				continue
+			}
+			name := entry.Name()
+			forgeName, ok := forgeNameFromDockerfile(name)
+			if !ok {
+				continue
+			}
+
+			filePath := filepath.Join(dir, name)
+			step := makeForgeStep(buildID, forgeName, filePath, config)
+			g.Steps = append(g.Steps, step)
+		}
+	}
+
+	return g, nil
 }
