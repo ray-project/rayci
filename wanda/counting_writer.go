aad06e1107c332f0fdc73960dac2c8029393d50b
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-08 10:20:57 -0700

    add a counting writer (#53)
    this is required for wrapping a tarball stream, which will be used for
    providing deterministic context input for a docker image build.
    
    wanda is the name of the new command line tool for building images with
    fast caches.

---
+++ wanda/counting_writer.go
@@ -0,0 +1,22 @@
+package wanda
+
+import (
+	"io"
+)
+
+type countingWriter struct {
+	w io.Writer
+	n int64
+}
+
+func newCountingWriter(w io.Writer) *countingWriter {
+	return &countingWriter{w: w}
+}
+
+// Write implements io.Writer; it writes to the underlying writer and counts
+// the number of bytes written.
+func (w *countingWriter) Write(buf []byte) (int, error) {
+	n, err := w.w.Write(buf)
+	w.n += int64(n)
+	return n, err
+}
---
+++ wanda/counting_writer_test.go
@@ -0,0 +1,19 @@
+package wanda
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestCountingWriter(t *testing.T) {
+	buf := new(bytes.Buffer)
+	w := newCountingWriter(buf)
+
+	io.WriteString(w, "hello")
+	io.WriteString(w, "world")
+
+	if w.n != 10 {
+		t.Errorf("expected 10 bytes written, got %d", w.n)
+	}
+}
