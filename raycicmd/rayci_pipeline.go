b05ac3d2421ce6f39a6b92d886ac3c5c98a84109
Lonnie Liu <lonnie@anyscale.com>
2023-08-08 02:54:05 +0000

    supports priority, and sets default priority to 1 for prs
    so that civ2 has higher priority than civ1.

--- raycicmd/rayci_pipeline.go
+++ raycicmd/rayci_pipeline.go
@@ -10,7 +10,7 @@ type pipelineGroup struct {
 var (
 	waitStepAllowedKeys    = []string{"wait", "continue_on_failure"}
 	commandStepAllowedKeys = []string{
-		"command", "commands",
+		"command", "commands", "priority",
 		"label", "name", "key", "depends_on", "soft_fail", "matrix",
 		"instance_type", "queue", "job_env",
 	}
