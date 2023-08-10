1035955bbf8b913fe964f2ad404e1f8db717b746
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-09 18:13:15 -0700

    add writerToReader (#54)
    this utility struct converts a writerTo to a reader. this will be used
    to pipe a dynamically generated tar stream a command line process for
    building containers.

---
+++ wanda/writer_to_reader_test.go
@@ -0,0 +1,39 @@
+package wanda
+
+import (
+	"testing"
+
+	"bytes"
+	"errors"
+	"io"
+)
+
+func TestWriterToReader(t *testing.T) {
+	buf := new(bytes.Buffer)
+	buf.WriteString("hello world")
+
+	r := newWriterToReader(buf)
+	bs, err := io.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(bs) != "hello world" {
+		t.Errorf("got %q, want %q", string(bs), "hello world")
+	}
+}
+
+type errWriterTo struct {
+	err error
+}
+
+func (w *errWriterTo) WriteTo(wt io.Writer) (int64, error) {
+	return 0, w.err
+}
+
+func TestWriterToReaderError(t *testing.T) {
+	writeErr := errors.New("write error")
+	r := newWriterToReader(&errWriterTo{err: writeErr})
+	if _, err := io.ReadAll(r); err != writeErr {
+		t.Errorf("got %v, want %v", err, writeErr)
+	}
+}
