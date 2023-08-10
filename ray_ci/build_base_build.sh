7d0c761242ff23aef0f29f7529d338970579493d
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-10 14:11:46 -0700

    cleanup cpu base building script (#56)
    get prepare to replace the building steps with wanda on civ2

--- ray_ci/build_base_build.sh
+++ ray_ci/build_base_build.sh
@@ -1,89 +1,98 @@
 #!/bin/bash
-set -e
+
+set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 
-cd "$RAY_REPO_DIR" || true
-
-if [ "$BUILDKITE_COMMIT" = "HEAD" ]; then
-  export BUILDKITE_COMMIT=$(git log -1 --format="%H")
-  echo "Resolved BUILDKITE_COMMIT to $BUILDKITE_COMMIT"
+if [[ "${RAY_REPO_DIR:-}" != "" ]]; then
+  cd "$RAY_REPO_DIR" || true
 fi
 
-# Convert / into _
-if [ -z "${BUILDKITE_PULL_REQUEST_BASE_BRANCH-}" ]; then
-  # In branches, use the BUILDKITE_BRANCH
-  export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_BRANCH//\//_}
-else
-  # In PRs, use the BUILDKITE_PULL_REQUEST_BASE_BRANCH
-  export BUILDKITE_BRANCH_CLEAN=${BUILDKITE_PULL_REQUEST_BASE_BRANCH//\//_}
+if [[ "${BUILDKITE_COMMIT:-HEAD}" == "HEAD" ]]; then
+  BUILDKITE_COMMIT="$(git rev-parse HEAD)"
 fi
+echo "BUILDKITE_COMMIT=$BUILDKITE_COMMIT"
 
-export DOCKER_IMAGE_BASE_TEST=$ECR_BASE_REPO:oss-ci-base_test_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG_TEST=$ECR_BASE_REPO:oss-ci-base_test_latest_$BUILDKITE_BRANCH_CLEAN
+ECR_BASE_REPO="${ECR_BASE_REPO:-029272617770.dkr.ecr.us-west-2.amazonaws.com/ci_base_images}"
 
-export DOCKER_IMAGE_BASE_BUILD=$ECR_BASE_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG_BUILD=$ECR_BASE_REPO:oss-ci-base_build_latest_$BUILDKITE_BRANCH_CLEAN
+DOCKER_IMAGE_BASE_TEST=$ECR_BASE_REPO:oss-ci-base_test_$BUILDKITE_COMMIT
+DOCKER_IMAGE_BASE_BUILD=$ECR_BASE_REPO:oss-ci-base_build_$BUILDKITE_COMMIT
+DOCKER_IMAGE_BASE_ML=$ECR_BASE_REPO:oss-ci-base_ml_$BUILDKITE_COMMIT
 
-export DOCKER_IMAGE_BASE_ML=$ECR_BASE_REPO:oss-ci-base_ml_$BUILDKITE_COMMIT
-export DOCKER_IMAGE_TAG_ML=$ECR_BASE_REPO:oss-ci-base_ml_latest_$BUILDKITE_BRANCH_CLEAN
-
+# DOCKER_IMAGE_BASE_TEST is used as build arg
+export DOCKER_IMAGE_BASE_TEST
 
 echo "--- :docker: Building base dependency image for TESTS :python:"
-date +"%Y-%m-%d %H:%M:%S"
 
-time docker build --progress=plain \
+docker build --progress=plain \
   --build-arg REMOTE_CACHE_URL \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
   -t "$DOCKER_IMAGE_BASE_TEST" \
-  -t "$DOCKER_IMAGE_TAG_TEST" \
   -f ci/docker/base.test.Dockerfile .
 
 echo "--- :docker: Building base dependency image for BUILDS :gear:"
-date +"%Y-%m-%d %H:%M:%S"
 
-time docker build --progress=plain \
+docker build --progress=plain \
   --build-arg REMOTE_CACHE_URL \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
   --build-arg DOCKER_IMAGE_BASE_TEST \
   -t "$DOCKER_IMAGE_BASE_BUILD" \
-  -t "$DOCKER_IMAGE_TAG_BUILD" \
   -f ci/docker/base.build.Dockerfile .
 
 echo "--- :docker: Building base dependency image for ML :airplane:"
-date +"%Y-%m-%d %H:%M:%S"
 
-time docker build --progress=plain \
+docker build --progress=plain \
   --build-arg REMOTE_CACHE_URL \
   --build-arg BUILDKITE_PULL_REQUEST \
   --build-arg BUILDKITE_COMMIT \
   --build-arg BUILDKITE_PULL_REQUEST_BASE_BRANCH \
   --build-arg DOCKER_IMAGE_BASE_TEST \
   -t "$DOCKER_IMAGE_BASE_ML" \
-  -t "$DOCKER_IMAGE_TAG_ML" \
   -f ci/docker/base.ml.Dockerfile .
 
-date +"%Y-%m-%d %H:%M:%S"
 
-if [ "${NO_PUSH}" = "1" ]; then
+if [[ "${NO_PUSH:-}" == "1" ]]; then
   echo "--- :exclamation: Not pushing the image as this is a local build only!"
   exit 0
 fi
 
 echo "--- :arrow_up: Pushing docker images to ECR"
-date +"%Y-%m-%d %H:%M:%S"
 
-time docker push "$DOCKER_IMAGE_BASE_BUILD"
-time docker push "$DOCKER_IMAGE_TAG_BUILD"
+# Convert / into _
+if [[ "${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}" != "" ]]; then
+  # In PRs, use the BUILDKITE_PULL_REQUEST_BASE_BRANCH
+  BRANCH_NAME="${BUILDKITE_PULL_REQUEST_BASE_BRANCH//\//_}"
+elif [[ "${BUILDKITE_BRANCH:-}" != "" ]]; then
+  # In branches, use the BUILDKITE_BRANCH
+  BRANCH_NAME="${BUILDKITE_BRANCH//\//_}"
+else
+  BRANCH_NAME="dev"
+fi
 
-time docker push "$DOCKER_IMAGE_BASE_TEST"
-time docker push "$DOCKER_IMAGE_TAG_TEST"
+DOCKER_IMAGE_TAG_TEST="${ECR_BASE_REPO}:oss-ci-base_test_latest_${BRANCH_NAME}"
+DOCKER_IMAGE_TAG_BUILD="${ECR_BASE_REPO}:oss-ci-base_build_latest_${BRANCH_NAME}"
+DOCKER_IMAGE_TAG_ML="${ECR_BASE_REPO}:oss-ci-base_ml_latest_${BRANCH_NAME}"
 
-time docker push "$DOCKER_IMAGE_BASE_ML"
-time docker push "$DOCKER_IMAGE_TAG_ML"
+echo "--- Push ci-base_test"
+docker push "$DOCKER_IMAGE_BASE_TEST"
 
-date +"%Y-%m-%d %H:%M:%S"
+echo "--- Push ci-base_build"
+docker push "$DOCKER_IMAGE_BASE_BUILD"
+
+echo "--- Push ci-base_ml"
+docker push "$DOCKER_IMAGE_BASE_ML"
+
+echo "--- Tagging aliases"
+
+docker tag "$DOCKER_IMAGE_BASE_BUILD" "$DOCKER_IMAGE_TAG_BUILD"
+docker push "$DOCKER_IMAGE_TAG_BUILD"
+
+docker tag "$DOCKER_IMAGE_BASE_TEST" "$DOCKER_IMAGE_TAG_TEST"
+docker push "$DOCKER_IMAGE_TAG_TEST"
+
+docker tag "$DOCKER_IMAGE_BASE_ML" "$DOCKER_IMAGE_TAG_ML"
+docker push "$DOCKER_IMAGE_TAG_ML"
