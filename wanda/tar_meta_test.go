fe53ee8eb943f5bb5dbba186e8d6dc70eb2fe0fc
Lonnie Liu <lonnie@anyscale.com>
2023-08-11 19:56:54 +0000

    add tar meta and tests

---
+++ wanda/tar_meta_test.go
@@ -0,0 +1,42 @@
+package wanda
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestTarMetaFromFileInfo(t *testing.T) {
+	tmp := t.TempDir()
+
+	for _, mod := range []int64{
+		0775, // bazel's sandbox does not support 777.
+		0644,
+		0600,
+		0400,
+		0700,
+	} {
+		path := filepath.Join(tmp, fmt.Sprintf("file-%d", mod))
+		data := []byte(fmt.Sprintf("%d", mod))
+		if err := os.WriteFile(path, data, os.FileMode(mod)); err != nil {
+			t.Fatalf("write file %q: %v", path, err)
+		}
+
+		info, err := os.Stat(path)
+		if err != nil {
+			t.Fatalf("stat %q: %v", path, err)
+		}
+
+		meta := tarMetaFromFileInfo(info)
+		if meta.Mode != mod {
+			t.Errorf("got mode %o, want %o", meta.Mode, mod)
+		}
+		if meta.GroupID != 0 {
+			t.Errorf("got group id %d, want 0", meta.GroupID)
+		}
+		if meta.UserID != 0 {
+			t.Errorf("got user id %d, want 0", meta.UserID)
+		}
+	}
+}
