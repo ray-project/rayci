1199162c0903e2478ac1685ba0f0a15c78e585a1
Lonnie Liu <lonnie@anyscale.com>
2023-08-12 02:55:51 +0000

    flags for wanda

--- wanda/wanda/main.go
+++ wanda/wanda/main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	workDir := flag.String("work_dir", ".", "root directory for the build")
+	docker := flag.String("docker", "", "path to the docker client binary")
+	cacheRepo := flag.String("cache_repo", "", "cache container repository")
+	readOnly := flag.Bool("read_only", false, "read-only cache repository")
+
 	flag.Parse()
 
 	args := flag.Args()
@@ -16,7 +21,14 @@ func main() {
 		log.Fatal("needs exactly one argument for the spec file")
 	}
 
-	if err := wanda.Build(args[0], nil); err != nil {
+	config := &wanda.ForgeConfig{
+		WorkDir:       *workDir,
+		DockerBin:     *docker,
+		CacheRepo:     *cacheRepo,
+		ReadOnlyCache: *readOnly,
+	}
+
+	if err := wanda.Build(args[0], config); err != nil {
 		log.Fatal(err)
 	}
 }
