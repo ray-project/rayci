df4149717121527cdb1c543f8d4c8452c3abed63
Lonnie Liu <lonnie@anyscale.com>
2023-08-15 17:23:04 +0000

    [wanda] add wanda spec file format and parsing
    wanda spec file specifies the input of a image build

---
+++ wanda/spec.go
@@ -0,0 +1,39 @@
+package wanda
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+// Spec is a specification for a container image.
+type Spec struct {
+	Name string `yaml:"name,omitempty"`
+
+	Tags []string `yaml:"tags"`
+
+	// Inputs
+	Froms      []string `yaml:"froms"`
+	Srcs       []string `yaml:"srcs,omitempty"`
+	Dockerfile string   `yaml:"dockerfile"`
+
+	BuildArgs []string `yaml:"build_args,omitempty"`
+}
+
+func parseSpecFile(f string) (*Spec, error) {
+	bs, err := os.ReadFile(f)
+	if err != nil {
+		return nil, fmt.Errorf("read file: %w", err)
+	}
+
+	spec := new(Spec)
+	dec := yaml.NewDecoder(bytes.NewReader(bs))
+	dec.KnownFields(true)
+	if err := dec.Decode(spec); err != nil {
+		return nil, fmt.Errorf("decode spec: %w", err)
+	}
+
+	return spec, nil
+}
