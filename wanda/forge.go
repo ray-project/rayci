7d3a3cc8d640e41d3c95922a8e09d2a78e4e09ed
Lonnie Liu <lonnie@anyscale.com>
2023-08-15 17:22:13 +0000

    split out spec

--- wanda/forge.go
+++ wanda/forge.go
@@ -1,10 +1,8 @@
 package wanda
 
 import (
-	"bytes"
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -12,38 +10,7 @@ import (
 	cranename "github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/daemon"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
-	"gopkg.in/yaml.v3"
 )
-
-// Spec is a specification for a container image.
-type Spec struct {
-	Name string `yaml:"name,omitempty"`
-
-	Tags []string `yaml:"tags"`
-
-	// Inputs
-	Froms      []string `yaml:"froms"`
-	Srcs       []string `yaml:"srcs,omitempty"`
-	Dockerfile string   `yaml:"dockerfile"`
-
-	BuildArgs []string `yaml:"build_args,omitempty"`
-}
-
-func parseSpecFile(f string) (*Spec, error) {
-	bs, err := os.ReadFile(f)
-	if err != nil {
-		return nil, fmt.Errorf("read file: %w", err)
-	}
-
-	spec := new(Spec)
-	dec := yaml.NewDecoder(bytes.NewReader(bs))
-	dec.KnownFields(true)
-	if err := dec.Decode(spec); err != nil {
-		return nil, fmt.Errorf("decode spec: %w", err)
-	}
-
-	return spec, nil
-}
 
 // Build builds a container image from the given specification file.
 func Build(specFile string, config *ForgeConfig) error {
