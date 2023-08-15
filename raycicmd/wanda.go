0cf48d3947d351a01ac61a3938b20453aa45b83c
Lonnie Liu <lonnie@anyscale.com>
2023-08-15 15:33:30 +0000

    .

--- raycicmd/wanda.go
+++ raycicmd/wanda.go
@@ -9,7 +9,7 @@ const runWandaURL = rawGitHubURL + "ray-project/rayci/lonnie-x/run_wanda.sh"
 
 var wandaCommands = []string{
 	fmt.Sprintf(`curl -sfL "%s" > /tmp/run_wanda.sh`, runWandaURL),
-	`RAYCI_BRANCH=lonnie-x /bin/bash /tmp/run_wanda.sh -rayci`,
+	`/bin/bash /tmp/run_wanda.sh -rayci`,
 }
 
 type wandaStep struct {
