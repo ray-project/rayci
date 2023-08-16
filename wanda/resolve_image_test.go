d24b0dfb357ae21e663175f1625bbf8bc5021de7
Lonnie Liu <lonnie@anyscale.com>
2023-08-16 07:30:48 +0000

    .

--- wanda/resolve_image_test.go
+++ wanda/resolve_image_test.go
@@ -1,12 +1,16 @@
 package wanda
 
 import (
+	"fmt"
 	"log"
+	"net/http/httptest"
 	"testing"
 
 	cranename "github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/registry"
 	"github.com/google/go-containerregistry/pkg/v1/daemon"
 	"github.com/google/go-containerregistry/pkg/v1/random"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 )
 
 func TestResolveLocalImage(t *testing.T) {
@@ -48,5 +52,45 @@ func TestResolveLocalImage(t *testing.T) {
 	dockerCmd := newDockerCmd("")
 	if err := dockerCmd.run("image", "rm", tagStr); err != nil {
 		t.Fatal("remove image: ", err)
+	}
+}
+
+func TestResolveRemoteImage(t *testing.T) {
+	cr := registry.New()
+	server := httptest.NewServer(cr)
+	defer server.Close()
+
+	addr := server.Listener.Addr().String()
+
+	random, err := random.Image(1024, 1)
+	if err != nil {
+		t.Fatal("create random image: ", err)
+	}
+
+	tagStr := fmt.Sprintf("%s/rayproject/wanda:resolve-test", addr)
+	ref, err := cranename.ParseReference(tagStr)
+	if err != nil {
+		t.Fatal("prase tag: ", err)
+	}
+
+	imageID, err := random.ConfigName()
+	if err != nil {
+		t.Fatal("get image id: ", err)
+	}
+
+	if err := remote.Write(ref, random); err != nil {
+		t.Fatal("save image to remote: ", err)
+	}
+
+	src, err := resolveRemoteImage("test-img", tagStr)
+	if err != nil {
+		t.Fatal("resolve image: ", err)
+	}
+
+	if src.name != "test-img" {
+		t.Errorf("got image name %q, want `test-img`", src.name)
+	}
+	if want := imageID.String(); want != src.id {
+		t.Errorf("got image id %q, want %q", src.id, want)
 	}
 }
