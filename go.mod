0828aa61d9b2b33fd4921d58e140ec30df7fcee5
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2023-08-16 10:41:39 -0700

    [wanda] add image source resolving (#80)
    to determine and pin the container base image input before building.

--- go.mod
+++ go.mod
@@ -11,16 +11,20 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20230124172434-306776ec8161 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.14.3 // indirect
+	github.com/docker/cli v24.0.0+incompatible // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/docker v24.0.0+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.1 // indirect
 	github.com/vbatts/tar-split v0.11.3 // indirect
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
---
+++ wanda/resolve_image.go
@@ -0,0 +1,61 @@
+package wanda
+
+import (
+	"fmt"
+
+	cranename "github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/daemon"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+func resolveLocalImage(name, ref string) (*imageSource, error) {
+	parsed, err := cranename.ParseReference(ref)
+	if err != nil {
+		return nil, fmt.Errorf("parse reference %s: %w", ref, err)
+	}
+
+	img, err := daemon.Image(parsed)
+	if err != nil {
+		return nil, fmt.Errorf("fetch image %s: %w", ref, err)
+	}
+	id, err := img.ConfigName()
+	if err != nil {
+		return nil, fmt.Errorf("get config name/id for %s: %w", ref, err)
+	}
+
+	return &imageSource{
+		name: name,
+		id:   id.String(),
+	}, nil
+}
+
+func resolveRemoteImage(name, ref string, opts ...remote.Option) (
+	*imageSource, error,
+) {
+	parsed, err := cranename.ParseReference(ref)
+	if err != nil {
+		return nil, fmt.Errorf("parse reference %s: %w", ref, err)
+	}
+	img, err := remote.Image(parsed, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("fetch image %s: %w", ref, err)
+	}
+
+	digest, err := img.Digest()
+	if err != nil {
+		return nil, fmt.Errorf("get digest for %s: %w", ref, err)
+	}
+
+	id, err := img.ConfigName()
+	if err != nil {
+		return nil, fmt.Errorf("get config name/id for %s: %w", ref, err)
+	}
+
+	src := parsed.Context().Digest(digest.String())
+
+	return &imageSource{
+		name: name,
+		id:   id.String(),
+		src:  src.String(),
+	}, nil
+}
