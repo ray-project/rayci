31682deb0134a8ea3025677528aded46fa210dc7
Lonnie Liu <lonnie@anyscale.com>
2023-08-17 06:51:23 +0000

    add a name prefix

--- ray_ci/build_base_build.sh
+++ ray_ci/build_base_build.sh
@@ -18,7 +18,10 @@ echo "--- Install wanda"
 curl -sL 'https://github.com/ray-project/rayci/releases/download/v0.1.3/wanda-linux-amd64' -o "$TMP_DIR/wanda"
 chmod +x "$TMP_DIR/wanda"
 
-WANDA=("$TMP_DIR/wanda")
+WANDA=(
+  "$TMP_DIR/wanda"
+  -name_prefix=cr.ray.io/rayproject/
+)
 
 echo "--- :docker: Building base dependency image for TESTS :python:"
 
