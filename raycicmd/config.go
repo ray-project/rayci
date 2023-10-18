941bc04f8c9746dbb4949a8dba60b698994afd51
Lonnie Liu <lonnie@anyscale.com>
2023-10-18 18:47:13 +0000

    add comments for config fields

--- raycicmd/config.go
+++ raycicmd/config.go
@@ -9,31 +9,75 @@ import (
 )
 
 type dockerPluginConfig struct {
+	// AllowMountDockerSocket sets if it is allowed for jobs to mount the
+	// buildkite agent. This should only be set for pipelines where all builds
+	// on the pipeline are trusted.
 	AllowMountBuildkiteAgent bool `yaml:"mount_buildkite_agent"`
 }
 
 type config struct {
+	// name is a private field that is only used for internal testing.
 	name string
 
+	// ArtifactsBucket specifies the S3 bucket to save build artifacts.
+	//
+	// Optional but highly recommended.
 	ArtifactsBucket string `yaml:"artifacts_bucket"`
 
-	CITemp      string `yaml:"ci_temp"`
-	CIWorkRepo  string `yaml:"ci_work_repo"`
+	// CITemp specifies the location to save temp files that can be used accross
+	// build jobs. Might not be usable on PR/premerge builds.
+	//
+	// Required.
+	CITemp string `yaml:"ci_temp"`
+
+	// CIWorkRepo is a container repository for saving wanda built images, which
+	// can be used as environment images for build jobs.
+	//
+	// Required.
+	CIWorkRepo string `yaml:"ci_work_repo"`
+
+	// ForgePrefix is a prefix for wanda-built forge images. This serves as a
+	// globally unique namespace for container image references, and ideally
+	// should be set to a domain that does not exist on the public Internet,
+	// to avoid the container builder from pulling public images when building.
+	// The prefix will be auto appended recognized to wanda built images before
+	// the wanda image name.
+	//
+	// Optional but highly recommended.
 	ForgePrefix string `yaml:"forge_prefix"`
 
+	// BuilderQueues is a mapping from job instance types to buildkite agent
+	// queues for builders. If not set, the agent queue will be omitted, and
+	// the default queue will be used.
+	//
+	// Optional but highly recommended.
 	BuilderQueues map[string]string `yaml:"builder_queues"`
-	RunnerQueues  map[string]string `yaml:"runner_queues"`
 
-	// Priority for builder command steps.
+	// RunnerQueues is a mapping from job instance types to buildkite agent
+	// queues for runners. If not set, the agent queue will be omitted, and the
+	// default queue will be used.
+	//
+	// Optional but highly recommended.
+	RunnerQueues map[string]string `yaml:"runner_queues"`
+
+	// BuilderPriority is the job priority for builder command steps.
+	//
+	// Optional.
 	BuilderPriority int `yaml:"builder_priority"`
 
-	// Default priority for runner command steps.
+	// RunnerPriority is the job priority for runner command steps.
+	//
+	// Optional.
 	RunnerPriority int `yaml:"runner_priority"`
 
 	// BuildkiteDir is the directory of buildkite pipeline files.
+	//
+	// Optional. Default is `[]string{".buildkite"}` .
 	BuildkiteDirs []string `yaml:"buildkite_dirs"`
 
 	// ForgeDir is the directory of forge Dockerfile files.
+	//
+	// Deprecated, don't use.
 	ForgeDirs []string `yaml:"forge_dirs"`
 
 	// Env is the environment variables to set for runner steps.
@@ -41,22 +85,31 @@ type config struct {
 
 	// HookEnvKeys is the list of environment variable keys to pass into
 	// build jobs from buildkite hooks.
+	//
+	// Optional.
 	HookEnvKeys []string `yaml:"hook_env_keys"`
 
-	// Command to populate the buildkite step tags. When this command is
-	// specified, a buildkite step is skipped if it is tagged by something that
-	// is not returned by this command.
+	// TagFilterCommand is a command invocation that populates the buildkite
+	// step tags. When this command is specified, a buildkite step is skipped
+	// if it is tagged by something that is not returned by this command.
 	//
 	// If the first arg (the binary) starts with "./", then it is treated as
 	// a file path relative the repsitory root, and it will be checked if the
 	// file exists. If the file does not exist, then the command is ignored
 	// and all steps will be executed.
+	//
+	// Optional.
 	TagFilterCommand []string `yaml:"tag_filter_command"`
 
 	// SkipTags is the list of tags that will always be skipped.
+	//
+	// Optional.
 	SkipTags []string `yaml:"skip_tags"`
 
-	// Additional docker plugin configs.
+	// DockerPlugin contains additional docker plugin configs, to fine tune
+	// the docker plugin's behavior.
+	//
+	// Optional.
 	DockerPlugin *dockerPluginConfig `yaml:"docker_plugin"`
 }
 
