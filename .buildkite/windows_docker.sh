5b1438368dd003298bfe647c5fae138a923dc4c2
Cuong Nguyen <128072568+can-anyscale@users.noreply.github.com>
2023-12-13 20:49:09 -0800

    [ci] add test for wanda run (#148)
    Add a test to pull wanda image and run
    
    Signed-off-by: can <can@anyscale.com>

--- .buildkite/windows_docker.sh
+++ .buildkite/windows_docker.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-mkdir -p /c/tools
-curl https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip > /c/tools/docker-17.09.0-ce.zip
-unzip /c/tools/docker-17.09.0-ce.zip -d /c/tools
-rm /c/tools/docker-17.09.0-ce.zip
+powershell .buildkite/fix-windows-container-networking.ps1
+pip install awscli
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 029272617770.dkr.ecr.us-west-2.amazonaws.com
