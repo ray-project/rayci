28e3f2670562d73ea47c9949fa72abd12958b4b6
Lonnie Liu <lonnie@anyscale.com>
2024-01-23 20:34:51 +0000

    convert command step into a converter too

--- raycicmd/converter.go
+++ raycicmd/converter.go
@@ -2,8 +2,6 @@ package raycicmd
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 )
 
 const windowsJobEnv = "WINDOWS"
@@ -16,6 +14,8 @@ type converter struct {
 	envMap map[string]string
 
 	stepConverters []stepConverter
+
+	defaultConverter stepConverter
 }
 
 func newConverter(config *config, info *buildInfo) *converter {
@@ -55,125 +55,9 @@ func newConverter(config *config, info *buildInfo) *converter {
 		blockConverter,
 		newWandaConverter(config, info, envMap),
 	}
+	c.defaultConverter = newCommandConverter(config, info, envMap)
 
 	return c
-}
-
-func (c *converter) mapAgent(instanceType string) (string, error) {
-	if instanceType == "" {
-		instanceType = "default"
-	}
-	if q, ok := c.config.RunnerQueues[instanceType]; ok {
-		return q, nil
-	}
-	return "", fmt.Errorf("unknown instance type %q", instanceType)
-}
-
-func (c *converter) jobEnvImage(name string) string {
-	if name == "" {
-		name = "forge"
-	}
-
-	return fmt.Sprintf("%s:%s-%s", c.config.CIWorkRepo, c.info.buildID, name)
-}
-
-const dockerPlugin = "docker#v5.8.0"
-const macosSandboxPlugin = "ray-project/macos-sandbox#v1.0.7"
-
-func (c *converter) convertRunner(step map[string]any) (map[string]any, error) {
-	if err := checkStepKeys(step, commandStepAllowedKeys); err != nil {
-		return nil, fmt.Errorf("check command step keys: %w", err)
-	}
-
-	queue, _ := stringInMapAnyKey(step, "queue", "instance_type")
-	if queue == "" {
-		queue = "default"
-	}
-	agentQueue, err := c.mapAgent(queue)
-	if err != nil {
-		return nil, fmt.Errorf("map agent: %w", err)
-	}
-
-	result := cloneMapExcept(step, commandStepDropKeys)
-
-	if agentQueue != skipQueue { // queue type not supported, skip.
-		result["agents"] = newBkAgents(agentQueue)
-	} else {
-		result["skip"] = true
-	}
-
-	result["retry"] = defaultRayRetry
-	result["timeout_in_minutes"] = defaultTimeoutInMinutes
-
-	priority, ok := step["priority"]
-	if !ok {
-		priority = c.config.RunnerPriority
-	}
-	if priority != 0 {
-		result["priority"] = priority
-	}
-
-	envMap := copyEnvMap(c.envMap)
-	result["env"] = envMap
-
-	envKeys := make(map[string]struct{})
-	for k := range envMap {
-		envKeys[k] = struct{}{}
-	}
-	for _, k := range c.config.HookEnvKeys {
-		envKeys[k] = struct{}{}
-	}
-	var envKeyList []string
-	for k := range envKeys {
-		envKeyList = append(envKeyList, k)
-	}
-	sort.Strings(envKeyList)
-
-	jobEnv, _ := stringInMap(step, "job_env")
-	dockerPluginConfig := &stepDockerPluginConfig{
-		extraEnvs: envKeyList,
-	}
-	if d := c.config.DockerPlugin; d != nil && d.AllowMountBuildkiteAgent {
-		v, _ := boolInMap(step, "mount_buildkite_agent")
-		dockerPluginConfig.mountBuildkiteAgent = v
-	}
-	publishPortsStr, _ := stringInMap(step, "docker_publish_tcp_ports")
-	if publishPortsStr != "" {
-		publishPorts := strings.Split(publishPortsStr, ",")
-		if len(publishPorts) > 0 {
-			dockerPluginConfig.publishTCPPorts = publishPorts
-		}
-	}
-	dockerNetwork, _ := stringInMap(step, "docker_network")
-	if dockerNetwork != "" {
-		dockerPluginConfig.network = dockerNetwork
-	}
-	v, _ := boolInMap(step, "mount_windows_artifacts")
-	dockerPluginConfig.mountWindowsArtifacts = v
-
-	if jobEnv == windowsJobEnv { // a special job env for windows
-		result["plugins"] = []any{map[string]any{
-			dockerPlugin: makeRayWindowsDockerPlugin(dockerPluginConfig),
-		}}
-		if dockerPluginConfig.mountWindowsArtifacts {
-			result["artifact_paths"] = windowsArtifactPaths
-		}
-	} else if jobEnv == macosJobEnv { // a special job env for macos
-		result["plugins"] = []any{map[string]any{
-			macosSandboxPlugin: map[string]string{
-				"deny-file-read": macosDenyFileRead,
-			},
-		}}
-	} else {
-		// default Linux Job env.
-		jobEnvImage := c.jobEnvImage(jobEnv)
-		result["plugins"] = []any{map[string]any{
-			dockerPlugin: makeRayDockerPlugin(jobEnvImage, dockerPluginConfig),
-		}}
-		result["artifact_paths"] = defaultArtifactPaths
-	}
-
-	return result, nil
 }
 
 func (c *converter) convertPipelineStep(step map[string]any) (
@@ -184,8 +68,7 @@ func (c *converter) convertPipelineStep(step map[string]any) (
 			return stepConverter.convert(step)
 		}
 	}
-
-	return c.convertRunner(step)
+	return c.defaultConverter.convert(step)
 }
 
 func (c *converter) convertPipelineGroup(g *pipelineGroup, filter *tagFilter) (
