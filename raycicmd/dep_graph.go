ecf120d521b90c12cbbc981cfa9ddcfb22e0bebd
Lonnie Liu <lonnie@anyscale.com>
2024-01-30 02:25:59 +0000

    address comments

--- raycicmd/dep_graph.go
+++ raycicmd/dep_graph.go
@@ -1,29 +1,37 @@
 package raycicmd
 
+// depNode is a node in a dependency graph.
 type depNode interface {
+	// deps lists all its dependencies.
 	deps() []string
-	mark() // mark if this node should be included
+
+	// mark marks this node as being included in the dependency graph.
+	mark()
 }
 
+// depGraph defines the interface for a dependency graph.
+// where each node has a unique name in the form of a string.
 type depGraph interface {
 	depNode(id string) depNode
 }
 
-func markDeps(g depGraph, set map[string]struct{}) {
-	hit := make(map[string]struct{})
-	for id := range set {
-		hit[id] = struct{}{}
+// markDeps marks all nodes in a dependency graph that are reachable from the
+// given set of starting nodes of starts.
+func markDeps(g depGraph, starts map[string]struct{}) {
+	hit := make(map[string]bool)
+	for id := range starts {
+		hit[id] = true
 	}
 
 	// For any included node, also include their dependencies.
-	this := make(map[string]struct{})
-	for id := range set {
-		this[id] = struct{}{}
+	this := make(map[string]bool)
+	for id := range starts {
+		this[id] = true
 	}
 
 	// BFS to include all dependencies.
 	for len(this) > 0 {
-		next := make(map[string]struct{})
+		next := make(map[string]bool)
 		for id := range this {
 			node := g.depNode(id)
 			if node == nil { // sliently skip non-existing nodes
@@ -32,9 +40,9 @@ func markDeps(g depGraph, set map[string]struct{}) {
 
 			node.mark()
 			for _, dep := range node.deps() {
-				if _, ok := hit[dep]; !ok {
-					next[dep] = struct{}{}
-					hit[dep] = struct{}{}
+				if !hit[dep] {
+					next[dep] = true
+					hit[dep] = true
 				}
 			}
 		}
