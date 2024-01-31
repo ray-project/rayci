43365b09f679a3472ff50992af9bd27174da6ec4
Lonnie Liu <lonnie@anyscale.com>
2024-01-31 04:25:52 +0000

    step nodes

--- raycicmd/step_nodes.go
+++ raycicmd/step_nodes.go
@@ -16,31 +16,31 @@ func newStepNodeSet() *stepNodeSet {
 	}
 }
 
-func (g *stepNodeSet) add(node *stepNode) {
-	g.nodes[node.id] = node
+func (s *stepNodeSet) add(node *stepNode) {
+	s.nodes[node.id] = node
 }
 
-func (g *stepNodeSet) addName(id, name string) error {
-	if _, ok := g.nameMap[name]; ok {
+func (s *stepNodeSet) addName(id, name string) error {
+	if _, ok := s.nameMap[name]; ok {
 		return fmt.Errorf("duplicate node key %q", name)
 	}
-	g.nameMap[name] = g.nodes[id]
+	s.nameMap[name] = s.nodes[id]
 	return nil
 }
 
-func (g *stepNodeSet) byName(name string) (*stepNode, bool) {
-	node, ok := g.nameMap[name]
+func (s *stepNodeSet) byName(name string) (*stepNode, bool) {
+	node, ok := s.nameMap[name]
 	return node, ok
 }
 
-func (g *stepNodeSet) depNode(id string) depNode {
-	n := g.nodes[id]
+func (s *stepNodeSet) depNode(id string) depNode {
+	n := s.nodes[id]
 	if n == nil {
 		return nil // For interface casting
 	}
 	return n
 }
 
-func (g *stepNodeSet) markDeps(starts map[string]struct{}) {
-	markDeps(g, starts)
+func (s *stepNodeSet) markDeps(starts map[string]struct{}) {
+	markDeps(s, starts)
 }
