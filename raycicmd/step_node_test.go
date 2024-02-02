71130162a295d279f5d1f9a8e5daccf4bbd5667b
Lonnie Liu <lonnie@anyscale.com>
2024-02-02 01:40:59 +0000

    .

--- raycicmd/step_node_test.go
+++ raycicmd/step_node_test.go
@@ -6,56 +6,6 @@ import (
 	"reflect"
 )
 
-func TestStepNodeDeps(t *testing.T) {
-	n := &stepNode{id: "mine"}
-
-	if got := n.deps(); len(got) != 0 {
-		t.Errorf("got deps %v, want empty list", n.deps())
-	}
-
-	n.addDep("foo")
-	want := []string{"foo"}
-	if got := n.deps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-
-	n.addDep("bar")
-	want = []string{"bar", "foo"}
-	if got := n.deps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-
-	n.addDep("foo")
-	if got := n.deps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
-func TestStepNodeReverseDeps(t *testing.T) {
-	n := &stepNode{id: "mine"}
-
-	if got := n.reverseDeps(); len(got) != 0 {
-		t.Errorf("got deps %v, want empty list", n.deps())
-	}
-
-	n.addReverseDep("foo")
-	want := []string{"foo"}
-	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-
-	n.addReverseDep("bar")
-	want = []string{"bar", "foo"}
-	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-
-	n.addReverseDep("foo")
-	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v, want %v", got, want)
-	}
-}
-
 func TestIntersects(t *testing.T) {
 	for _, test := range []struct {
 		set1 []string
@@ -142,5 +92,55 @@ func TestStepNodeHasTagIn(t *testing.T) {
 				got, test.want,
 			)
 		}
+	}
+}
+
+func TestStepNodeDeps(t *testing.T) {
+	n := &stepNode{id: "mine"}
+
+	if got := n.deps(); len(got) != 0 {
+		t.Errorf("got deps %v, want empty list", n.deps())
+	}
+
+	n.addDep("foo")
+	want := []string{"foo"}
+	if got := n.deps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	n.addDep("bar")
+	want = []string{"bar", "foo"}
+	if got := n.deps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	n.addDep("foo")
+	if got := n.deps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestStepNodeReverseDeps(t *testing.T) {
+	n := &stepNode{id: "mine"}
+
+	if got := n.reverseDeps(); len(got) != 0 {
+		t.Errorf("got deps %v, want empty list", n.deps())
+	}
+
+	n.addReverseDep("foo")
+	want := []string{"foo"}
+	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	n.addReverseDep("bar")
+	want = []string{"bar", "foo"}
+	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+
+	n.addReverseDep("foo")
+	if got := n.reverseDeps(); !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
 	}
 }
