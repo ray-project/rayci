05fd149568a7278e1621516dd6e32c3aa89b624c
Lonnie Liu <lonnie@anyscale.com>
2024-11-12 09:36:09 -0800

    [reefd] add http server skeleton
    for starting a service

---
+++ reefd/serve.go
@@ -0,0 +1,33 @@
+package reefd
+
+import (
+	"io"
+	"net/http"
+)
+
+// Config contains the configuration for the running the server.
+type Config struct {
+	Addr string
+}
+
+type server struct {
+	config *Config
+}
+
+func newServer(c *Config) *server {
+	return &server{config: c}
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World!")
+}
+
+// Serve runs the server.
+func Serve(addr string, c *Config) error {
+	s := newServer(c)
+	httpServer := &http.Server{
+		Addr:    addr,
+		Handler: s,
+	}
+	return httpServer.ListenAndServe()
+}
