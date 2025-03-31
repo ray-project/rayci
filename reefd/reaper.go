943c8ede6adadce55af459bd3a4ffaffff44346e
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-03-31 11:45:30 -0700

    [reefd] add windows instance reaper (#261)
    terminates windows instance that runs for too long
    
    ---------
    
    Signed-off-by: Lonnie Liu <lonnie@anyscale.com>

---
+++ reefd/reaper.go
@@ -0,0 +1,98 @@
+package reefd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sort"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+type reaper struct {
+	ec2     ec2Client
+	nowFunc func() time.Time
+}
+
+func newReaper(ec2 ec2Client) *reaper {
+	return &reaper{ec2: ec2}
+}
+
+func (r *reaper) now() time.Time {
+	if r.nowFunc != nil {
+		return r.nowFunc()
+	}
+	return time.Now()
+}
+
+func (r *reaper) setNowFunc(f func() time.Time) {
+	r.nowFunc = f
+}
+
+func (r *reaper) listDeadWindowsInstances(ctx context.Context) ([]string, error) {
+	filters := []types.Filter{{
+		Name:   aws.String("tag:BuildkiteQueue"),
+		Values: []string{"*windows*"},
+	}, {
+		Name: aws.String("instance-state-code"),
+		Values: []string{
+			"0",  // pending
+			"16", // running
+		},
+	}}
+	const maxResults = 500
+	input := &ec2.DescribeInstancesInput{
+		Filters:    filters,
+		MaxResults: aws.Int32(maxResults),
+	}
+	result, err := r.ec2.DescribeInstances(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("describe instances: %w", err)
+	}
+
+	const instanceAgeLimit = -4 * time.Hour
+
+	cut := r.now().Add(instanceAgeLimit)
+
+	var instances []string
+	for _, r := range result.Reservations {
+		for _, i := range r.Instances {
+			if i.LaunchTime.Before(cut) {
+				instances = append(instances, *i.InstanceId)
+			}
+		}
+	}
+
+	sort.Strings(instances)
+
+	return instances, nil
+}
+
+func (r *reaper) terminateInstances(ctx context.Context, ids []string) error {
+	if len(ids) == 0 {
+		return nil
+	}
+	input := &ec2.TerminateInstancesInput{InstanceIds: ids}
+	_, err := r.ec2.TerminateInstances(ctx, input)
+	return err
+}
+
+func (r *reaper) listAndReapDeadWindowsInstances(ctx context.Context) (int, error) {
+	ids, err := r.listDeadWindowsInstances(ctx)
+	if err != nil {
+		return 0, err
+	}
+	if len(ids) == 0 {
+		return 0, nil
+	}
+
+	log.Printf("terminating %d instances: %v", len(ids), ids)
+	if err := r.terminateInstances(ctx, ids); err != nil {
+		return 0, err
+	}
+
+	return len(ids), nil
+}
