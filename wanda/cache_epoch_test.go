8f1dec010707c8966ef0ae061cf1303c55b27d84
Lonnie Liu <lonnie@anyscale.com>
2025-06-02 18:18:08 +0000

    rename to morning of

--- wanda/cache_epoch_test.go
+++ wanda/cache_epoch_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestDefaultCacheEpoch(t *testing.T) {
-	date := func(year int, month time.Month, day int) time.Time {
+	morningOf := func(year int, month time.Month, day int) time.Time {
 		return time.Date(year, month, day, 8, 0, 0, 0, sfoAround)
 	}
 
@@ -16,20 +16,20 @@ func TestDefaultCacheEpoch(t *testing.T) {
 		expected string
 	}{{
 		name:     "saturday",
-		now:      date(2025, time.May, 31), // Saturday
+		now:      morningOf(2025, time.May, 31), // Saturday
 		expected: "202522",
 	}, {
 		name:     "sunday",
-		now:      date(2025, time.June, 1), // Sunday
+		now:      morningOf(2025, time.June, 1), // Sunday
 		expected: "202523",
 	}, {
 		name:     "monday",
-		now:      date(2025, time.June, 2), // Monday
+		now:      morningOf(2025, time.June, 2), // Monday
 		expected: "202523",
 	}, {
 		name:     "year boundary",
-		now:      date(2023, time.December, 31), // Sunday
-		expected: "202401",                      // First full week of 2024
+		now:      morningOf(2023, time.December, 31), // Sunday
+		expected: "202401",                           // First full week of 2024
 	}}
 
 	for _, test := range tests {
