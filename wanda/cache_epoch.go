28231454bbcf0fdab28f63adbf0778fbb5b5555b
Lonnie Liu <95255098+aslonnie@users.noreply.github.com>
2025-06-02 17:29:50 -0700

    shift cache epoch by 1 day (#287)
    so that monday morning feels faster

---
+++ wanda/cache_epoch.go
@@ -0,0 +1,20 @@
+package wanda
+
+import (
+	"fmt"
+	"time"
+)
+
+var sfoAround = time.FixedZone("SFO", -7*60*60)
+
+func defaultCacheEpoch(nowFunc func() time.Time) string {
+	now := nowFunc().In(sfoAround)
+	// When it is Sunday, we use the next week's epoch.
+	year, week := now.Add(24 * time.Hour).ISOWeek()
+	return fmt.Sprintf("%d%02d", year, week)
+}
+
+// DefaultCacheEpoch returns the default cache epoch.
+func DefaultCacheEpoch() string {
+	return defaultCacheEpoch(time.Now)
+}
---
+++ wanda/cache_epoch_test.go
@@ -0,0 +1,47 @@
+package wanda
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDefaultCacheEpoch(t *testing.T) {
+	morningOf := func(year int, month time.Month, day int) time.Time {
+		return time.Date(year, month, day, 8, 0, 0, 0, sfoAround)
+	}
+
+	tests := []struct {
+		name     string
+		now      time.Time
+		expected string
+	}{{
+		name:     "saturday",
+		now:      morningOf(2025, time.May, 31), // Saturday
+		expected: "202522",
+	}, {
+		name:     "sunday",
+		now:      morningOf(2025, time.June, 1), // Sunday
+		expected: "202523",
+	}, {
+		name:     "monday",
+		now:      morningOf(2025, time.June, 2), // Monday
+		expected: "202523",
+	}, {
+		name:     "year boundary",
+		now:      morningOf(2023, time.December, 31), // Sunday
+		expected: "202401",                           // First full week of 2024
+	}}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			nowFunc := func() time.Time {
+				return test.now
+			}
+
+			got := defaultCacheEpoch(nowFunc)
+			if got != test.expected {
+				t.Errorf("DefaultCacheEpoch() = %v, want %v", got, test.expected)
+			}
+		})
+	}
+}
