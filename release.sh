ebe2648d0b6a8d6bade6cd6c51343ca696f5ea19
andrew <andrew@anyscale.com>
2026-01-12 11:22:25 -0800

    cd: refactor release.sh
    Refactoring release.sh to make it easier to add rayapp as a new target.
    
    Topic: refactor-release
    
    Signed-off-by: andrew <andrew@anyscale.com>

--- release.sh
+++ release.sh
@@ -5,23 +5,22 @@ set -euxo pipefail
 mkdir -p _release
 rm -f _release/*
 
-function build_rayci {
-	RAYCI_OS="$1"
-	RAYCI_ARCH="$2"
+build_go() {
+  local name="$1"
+  local pkg="$2"
+  local os="$3"
+  local arch="$4"
 
-	GOOS="$RAYCI_OS" GOARCH="$RAYCI_ARCH" go build -trimpath -o "_release/rayci-${RAYCI_OS}-${RAYCI_ARCH}" .
+  GOOS="$os" GOARCH="$arch" \
+    go build -trimpath -o "_release/${name}-${os}-${arch}" "$pkg"
 }
 
-function build_wanda {
-	RAYCI_OS="$1"
-	RAYCI_ARCH="$2"
+build_rayci()  { build_go rayci  .             "$1" "$2"; }
+build_wanda()  { build_go wanda  ./wanda/wanda "$1" "$2"; }
 
-	GOOS="$RAYCI_OS" GOARCH="$RAYCI_ARCH" go build -trimpath -o "_release/wanda-${RAYCI_OS}-${RAYCI_ARCH}" ./wanda/wanda
-}
+build_rayci  linux   amd64
 
-build_rayci linux amd64
-
-build_wanda darwin arm64
-build_wanda linux amd64
-build_wanda linux arm64
-build_wanda windows amd64
+build_wanda  darwin  arm64
+build_wanda  linux   amd64
+build_wanda  linux   arm64
+build_wanda  windows amd64
