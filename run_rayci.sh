2748140142505b2699b83002b174949be3b2c64c
sai.miduthuri <sai.miduthuri@anyscale.com>
2026-05-07 07:33:37 -0700

    Revert "Clone"
    This reverts commit 3ab6afc10d9663db0303130ad2f1058f0ffbbb49.

--- run_rayci.sh
+++ run_rayci.sh
@@ -16,25 +16,17 @@ TMP_DIR="$(mktemp -d)"
 
 # Legacy path; build from source.
 
-if [ ! -f "/tmp/rayci" ]; then
-  RAYCI_BRANCH="select-prefix-token"
+RAYCI_BRANCH="select-prefix-token"
 
-  echo "--- Build rayci locally"
+echo "--- Build rayci locally"
 
-  git clone https://github.com/ray-project/rayci.git -b "$RAYCI_BRANCH" "$TMP_DIR/rayci-repo"
-  cd "$TMP_DIR/rayci-repo"
-
-  readonly GO_VERSION=1.24.5
-  curl -sfL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -xzf - -C "$TMP_DIR"
-  export GOROOT="$TMP_DIR/go"
-  export GOPATH="$TMP_DIR/gopath"
-  export GOPRIVATE="github.com/ray-project/rayci"
-  "$TMP_DIR/go/bin/go" build -o "/tmp/rayci" .
-  chmod +x /tmp/rayci
-
-  cd -
-fi
+readonly GO_VERSION=1.24.5
+curl -sfL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -xzf - -C "$TMP_DIR"
+export GOROOT="$TMP_DIR/go"
+export GOPATH="$TMP_DIR/gopath"
+export GOPRIVATE="github.com/ray-project/rayci"
+"$TMP_DIR/go/bin/go" build -o "$TMP_DIR/rayci" .
 
 echo "--- Run rayci"
 
-exec "/tmp/rayci" "$@"
+exec "$TMP_DIR/rayci" "$@"
